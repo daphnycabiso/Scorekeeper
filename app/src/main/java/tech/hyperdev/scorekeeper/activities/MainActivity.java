478ace1208f55c8ab512a17ec2688c1c4cf2a58e
Daphny Cabiso <daphnycabiso@bsit.usjr.edu.ph>
2017-07-18 21:56:07 +0800

    Modify team names of each score fragment

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,21 +1,41 @@
 package tech.hyperdev.scorekeeper.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ScoreFragment mScoreFragment;
+
+    FrameLayout container1;
+    FrameLayout container2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_score);
+        setContentView(R.layout.activity_main);
 
-        mScoreFragment = ScoreFragment.newInstance();
+        if(savedInstanceState == null) {
+
+            container1 = (FrameLayout) findViewById(R.id.team1Container);
+            container2 = (FrameLayout) findViewById(R.id.team2Container);
+
+            Fragment score_one = ScoreFragment.newInstance("Team 1");
+            Fragment score_two = ScoreFragment.newInstance("Team 2");
+
+            FragmentTransaction one = getSupportFragmentManager().beginTransaction();
+            one.add(R.id.team1Container, score_one).commit();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.team2Container, score_two).commit();
+
+        }
+
     }
 
 
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
@@ -12,6 +13,12 @@ import tech.hyperdev.scorekeeper.R;
  * A simple {@link Fragment} subclass.
  */
 public class ScoreFragment extends Fragment {
+
+
+    public View view;
+    public String name;
+    public int fscore;
+    public String arg;
 
 
     public ScoreFragment() {
@@ -27,10 +34,33 @@ public class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        view = inflater.inflate(R.layout.fragment_score, container, false);
+        readBundle(getArguments());
+
+        TextView tvname = (TextView) view.findViewById(R.id.tvTeamName);
+        tvname.setText(name + "");
+
+        return view;
+
     }
 
 
+
+
+    public static ScoreFragment newInstance(String name){
+        Bundle bundle = new Bundle();
+        bundle.putString("name",name);
+
+        ScoreFragment fragment = new ScoreFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    private void readBundle(Bundle bundle) {
+        if (bundle != null) {
+            name = bundle.getString("name");
+        }
+    }
 
 
 }
